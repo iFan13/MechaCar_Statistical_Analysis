67bcd60e76837eeebfa8880f49a5e4375cb7f8c4
Ian Fan <ian.fan13@gmail.com>
2021-04-10 00:16:05 -0400

    Deliverable 1 & 2 Technical

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,11 @@
-library()
+library(dbplyr)
+library(tidyverse)
+
 MechaCar<-read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar))
+
+SuspensionCoil <-read.csv(file='Suspension_Coil.csv',header=TRUE, ",", check.names = F, stringsAsFactors = F)
+total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance = var(PSI), SD = sd(PSI))
+
